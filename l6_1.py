a733e731b2c2831baefcdb7bb8c650c244e074fd
Krvet <148284532+Krvet@users.noreply.github.com>
2024-01-09 16:15:33 +0300

    Add files via upload

---
+++ l6_1.py
@@ -0,0 +1,8 @@
+n = int(input())
+k = 0
+
+for i in range(n):
+    l = int(input())
+    if l == 0:
+        k += 1
+print(k)
---
+++ l6_3.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+
+a <= b
+
+for i in range(a, b):
+    if i % 2 == 0:
+        print(i, end=" ")
